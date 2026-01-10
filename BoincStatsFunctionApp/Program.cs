60f4df7fe10844d79f8e97fe87957dfec72308de
Ádám Sarok <sarok.adam@gmail.com>
2026-01-10 14:43:47 +0100

    mi for storage

--- BoincStatsFunctionApp/Program.cs
+++ BoincStatsFunctionApp/Program.cs
@@ -1,4 +1,5 @@
 using Azure.Data.Tables;
+using Azure.Identity;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,12 +9,14 @@ var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-// Register TableServiceClient
+// Register TableServiceClient with managed identity
 builder.Services.AddSingleton(sp =>
 {
-    var connectionString = Environment.GetEnvironmentVariable("STORAGE_CONNECTION_STRING") 
-        ?? throw new InvalidOperationException("STORAGE_CONNECTION_STRING not found");
-    return new TableServiceClient(connectionString);
+    var storageName = Environment.GetEnvironmentVariable("StorageName") 
+        ?? throw new InvalidOperationException("StorageName environment variable not found");
+    
+    var tableServiceUri = new Uri($"https://{storageName}.table.core.windows.net/");
+    return new TableServiceClient(tableServiceUri, new DefaultAzureCredential());
 });
 
 builder.Services
